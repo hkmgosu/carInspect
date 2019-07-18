b0b9d8c815459c4beb14606130002bcad7575050
hkmgosu <erick.quiroz@live.com>
2019-07-18 12:16:11 -0400

    dev and prod env fixed updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,52 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { AppVersion } from '@ionic-native/app-version';
-import { Network } from '@ionic-native/network';
-import { NetworkService } from '../providers/network/network';
-import { PhotoViewer } from '@ionic-native/photo-viewer';
-import { HttpModule } from '@angular/http';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { TimeoutInterceptor } from '../providers/interceptor/timeout';
-import { Camera } from '@ionic-native/camera';
-import { FileTransfer } from '@ionic-native/file-transfer';
-import { AutoresizeDirective } from '../directives/autosize/autosize';
-import { UppercaseDirective } from '../directives/uppercase-input/uppercase';
-import { PipesModule } from '../pipes/pipes.module';
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { LoginPage } from '../pages/login/login';
-import { InspeccionesPage } from '../pages/inspecciones/inspecciones';
-import { InspeccionPage } from '../pages/inspeccion/inspeccion';
-import { FormularioPage } from '../pages/formulario/formulario';
-import { CrearinspeccionPage } from '../pages/crearinspeccion/crearinspeccion';
-import { GaleriafotoPage } from '../pages/galeriafoto/galeriafoto';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { AppVersion } from "@ionic-native/app-version";
+import { Network } from "@ionic-native/network";
+import { NetworkService } from "../providers/network/network";
+import { PhotoViewer } from "@ionic-native/photo-viewer";
+import { HttpModule } from "@angular/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { TimeoutInterceptor } from "../providers/interceptor/timeout";
+import { Camera } from "@ionic-native/camera";
+import { FileTransfer } from "@ionic-native/file-transfer";
+import { AutoresizeDirective } from "../directives/autosize/autosize";
+import { UppercaseDirective } from "../directives/uppercase-input/uppercase";
+import { PipesModule } from "../pipes/pipes.module";
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { DataProvider } from "../providers/data/data";
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { DataProvider } from '../providers/data/data';
+import { MyApp } from "./app.component";
+import { HomePage } from "../pages/home/home";
+import { LoginPage } from "../pages/login/login";
+import { InspeccionesPage } from "../pages/inspecciones/inspecciones";
+import { InspeccionPage } from "../pages/inspeccion/inspeccion";
+import { FormularioPage } from "../pages/formulario/formulario";
+import { CrearinspeccionPage } from "../pages/crearinspeccion/crearinspeccion";
+
+import { GaleriafotoPage } from "../pages/galeriafoto/galeriafoto";
+import { LoginPageModule } from "../pages/login/login.module";
+import { InspeccionesPageModule } from "../pages/inspecciones/inspecciones.module";
+import { InspeccionPageModule } from "../pages/inspeccion/inspeccion.module";
+import { FormularioPageModule } from "../pages/formulario/formulario.module";
+import { CrearinspeccionPageModule } from "../pages/crearinspeccion/crearinspeccion.module";
+import { GaleriafotoPageModule } from "../pages/galeriafoto/galeriafoto.module";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    LoginPage,
-    InspeccionesPage,
-    InspeccionPage,
-    FormularioPage,
-    CrearinspeccionPage,
-    GaleriafotoPage,
-    AutoresizeDirective,
-    UppercaseDirective
-  ],
+  declarations: [MyApp, HomePage, AutoresizeDirective, UppercaseDirective],
   imports: [
     BrowserModule,
     HttpModule,
     HttpClientModule,
     PipesModule,
-    IonicModule.forRoot(MyApp),
+    LoginPageModule,
+    InspeccionesPageModule,
+    InspeccionPageModule,
+    FormularioPageModule,
+    CrearinspeccionPageModule,
+    GaleriafotoPageModule,
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -68,7 +70,7 @@ import { DataProvider } from '../providers/data/data';
     FileTransfer,
     DataProvider,
     [{ provide: HTTP_INTERCEPTORS, useClass: TimeoutInterceptor, multi: true }],
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
--- src/providers/data/data.ts
+++ src/providers/data/data.ts
@@ -1,211 +1,266 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { LoadingController, AlertController } from 'ionic-angular';
-import 'rxjs/add/operator/map';
-import { env } from '@app/env';
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { LoadingController, AlertController } from "ionic-angular";
+import "rxjs/add/operator/map";
+import { env } from "../../env/env.dev";
 
 @Injectable()
 export class DataProvider {
-  private loader:any;
+  private loader: any;
   inspecciones: any = [];
   link: string;
   headers: any;
   options: any;
-  private _isDev: boolean = ((<any>window)['IonicDevServer'] != undefined);
+  private _isDev: boolean = (<any>window)["IonicDevServer"] != undefined;
   constructor(
     public http: HttpClient,
     private loadingCtrl: LoadingController,
     private alertCtrl: AlertController
   ) {
-    this.link = this._isDev ? '/dev/' : 'http://18.217.243.101/';
+    this.link = this._isDev ? "/dev/" : env.API_URL;
     console.log(this.link);
   }
-  getAllInspecciones(){
+  getAllInspecciones() {
     this.presentLoading();
-    const user = JSON.parse(localStorage.getItem('info_user'));
+    const user = JSON.parse(localStorage.getItem("info_user"));
     return new Promise((resolve, reject) => {
-      this.http.get(env.API_URL+'inspeccion', {headers:{code:user.code},params:{id_user:user.id_user,status:'0'}})
-      .subscribe(res => {
-        this.loader.dismiss();
-        resolve(res);
-      }, (err) => {
-        this.loader.dismiss();
-        this.handleError(err);
-        reject(err);
-      });
+      this.http
+        .get(env.API_URL + "inspeccion", {
+          headers: { code: user.code },
+          params: { id_user: user.id_user, status: "0" }
+        })
+        .subscribe(
+          res => {
+            this.loader.dismiss();
+            console.log("inspecciones", res);
+            resolve(res);
+          },
+          err => {
+            this.loader.dismiss();
+            this.handleError(err);
+            reject(err);
+          }
+        );
     });
   }
-  getFotosInspeccion(id_ins){
-    let info = JSON.parse(localStorage.getItem('info_user'));
+  getFotosInspeccion(id_ins) {
+    let info = JSON.parse(localStorage.getItem("info_user"));
     const body = new HttpParams()
-      .set('id_user', info.id_user)
-      .set('code', info.code)
-      .set('id_ins',id_ins)
+      .set("id_user", info.id_user)
+      .set("code", info.code)
+      .set("id_ins", id_ins);
     return new Promise((resolve, reject) => {
-      this.http.post(this.link+'fotos_inspeccion.php', body)
-      .subscribe(res => {
-        resolve(res);
-      }, (err) => {
-        reject(err);
-      });
+      this.http.post(env.API_URL + "fotos_inspeccion.php", body).subscribe(
+        res => {
+          resolve(res);
+        },
+        err => {
+          reject(err);
+        }
+      );
     });
   }
-  createInspeccion(item){
+  createInspeccion(item) {
     this.presentLoading();
-    let info = JSON.parse(localStorage.getItem('info_user'));
-    const body = new HttpParams()
-      .set('id_user', info.id_user)
-      .set('code', info.code)
-      .set('patente', item.patente)
-      .set('marca', item.marca)
-      .set('modelo', item.modelo)
-      .set('correo',item.correo)
-      .set('nombre',item.nombre)
-      .set('apellido',item.apellido)
-    return new Promise((resolve) => {
-      this.http.post(this.link+'crear_insp_app.php', body)
-      .subscribe(res => {
-        this.loader.dismiss();
-        resolve(res);
-      }, (err) => {
-        this.loader.dismiss();
-        this.handleError(err);
-      });
+    let info = JSON.parse(localStorage.getItem("info_user"));
+    const body = {
+      inspector: { id: info.id_user },
+      cliente: {
+        nombre: item.nombre,
+        apellido: item.apellido,
+        correo: item.correo
+      },
+      vehiculo: {
+        patente: item.patente,
+        marca: item.marca,
+        modelo: item.modelo
+      }
+    };
+    console.log("params", body);
+    return new Promise(resolve => {
+      this.http
+        .post(env.API_URL + "inspeccion", body, {
+          headers: { code: info.code }
+        })
+        .subscribe(
+          res => {
+            this.loader.dismiss();
+            console.log("guardar inspeccion", res);
+            resolve(res);
+          },
+          err => {
+            this.loader.dismiss();
+            console.log("error guardar inspeccion", err);
+            this.handleError(err);
+          }
+        );
     });
   }
-  uploadPhoto2Inspeccion({id_ins,base64img}){
-    let info = JSON.parse(localStorage.getItem('info_user'));
+  uploadPhoto2Inspeccion({ id_ins, base64img }) {
+    let info = JSON.parse(localStorage.getItem("info_user"));
     let encodedImg = encodeURIComponent(base64img);
     const body = new HttpParams()
-      .set('id_user', info.id_user)
-      .set('id_ins', id_ins)
-      .set('code', info.code)
-      .set('image', encodedImg);
-    return new Promise((resolve) => {
-      this.http.post(this.link+'upload.php', body)
-      .subscribe((res:any) => {
-        resolve(res);
-      }, (err) => {
-        this.handleError(err);
-      });
+      .set("id_user", info.id_user)
+      .set("id_ins", id_ins)
+      .set("code", info.code)
+      .set("image", encodedImg);
+    return new Promise(resolve => {
+      this.http.post(env.API_URL + "upload.php", body).subscribe(
+        (res: any) => {
+          resolve(res);
+        },
+        err => {
+          this.handleError(err);
+        }
+      );
     });
   }
-  updateDataInspeccion({valor,campo,id_ins}){
-    const user = JSON.parse(localStorage.getItem('info_user'));
+  updateDataInspeccion({ valor, campo, id_ins }) {
+    const user = JSON.parse(localStorage.getItem("info_user"));
     return new Promise((resolve, reject) => {
-      this.http.put(env.API_URL+'inspeccion'+'/'+id_ins,{[campo]:valor}, {headers:{code:user.code}})
-      .subscribe(res => {
-        console.log('Respuesta API');
-        console.log(res);
-        resolve(res);
-      }, (err) => {
-        //reject(err);
-        this.handleErr(err);
-      });
+      this.http
+        .put(
+          env.API_URL + "inspeccion" + "/" + id_ins,
+          { [campo]: valor },
+          { headers: { code: user.code } }
+        )
+        .subscribe(
+          res => {
+            console.log("Respuesta API");
+            console.log(res);
+            resolve(res);
+          },
+          err => {
+            //reject(err);
+            this.handleErr(err);
+          }
+        );
     });
   }
-  deletePhotoInspeccion(id_img){
-    let info = JSON.parse(localStorage.getItem('info_user'));
+  deletePhotoInspeccion(id_img) {
+    let info = JSON.parse(localStorage.getItem("info_user"));
     const body = new HttpParams()
-      .set('id_user', info.id_user)
-      .set('code', info.code)
-      .set('id_img', id_img);
+      .set("id_user", info.id_user)
+      .set("code", info.code)
+      .set("id_img", id_img);
     return new Promise((resolve, reject) => {
-      this.http.post(this.link+'eliminar_foto.php', body)
-      .subscribe(res => {
-        resolve(res);
-      }, (err) => {
-        reject(err);
-      });
+      this.http.post(env.API_URL + "eliminar_foto.php", body).subscribe(
+        res => {
+          resolve(res);
+        },
+        err => {
+          reject(err);
+        }
+      );
     });
   }
-  login(email, password){
+  login(email, password) {
     this.presentLoading();
-    const data = {username:email,password:password};
-    return new Promise((resolve) => {
-      this.http.post(env.API_URL+'user/login', data)
-      .subscribe(res => {
-        this.loader.dismiss();
-        resolve(res);
-      }, (err) => {
-        this.loader.dismiss();
-        this.handleErr(err);
-      });
+    const data = { username: email, password: password };
+    return new Promise(resolve => {
+      this.http.post(env.API_URL + "user/login", data).subscribe(
+        res => {
+          this.loader.dismiss();
+          resolve(res);
+        },
+        err => {
+          this.loader.dismiss();
+          this.handleErr(err);
+        }
+      );
     });
   }
   private presentLoading(msg?) {
     this.loader = this.loadingCtrl.create({
-        content: msg ? msg : "Por favor, espere..."
+      content: msg ? msg : "Por favor, espere..."
     });
     this.loader.present();
   }
-  private showAlert(title,subtitle) {
+  private showAlert(title, subtitle) {
     const alert = this.alertCtrl.create({
-        title: title,
-        subTitle: subtitle,
-        buttons: ['OK']
+      title: title,
+      subTitle: subtitle,
+      buttons: ["OK"]
     });
     alert.present();
   }
-  private handleError(e){
+  private handleError(e) {
     console.log(e);
-    if(e.status){
-      switch(e.status){
-        case 0:{
-          this.showAlert('Error!','Usted está trabajando sin internet. Revise su conexión.');
+    if (e.status) {
+      switch (e.status) {
+        case 0: {
+          this.showAlert(
+            "Error!",
+            "Usted está trabajando sin internet. Revise su conexión."
+          );
           break;
         }
-        case 404:{
-          this.showAlert('Error!','Recurso no encontrado. Contacte al administrador.');
+        case 404: {
+          this.showAlert(
+            "Error!",
+            "Recurso no encontrado. Contacte al administrador."
+          );
           break;
         }
         case 500: {
-          this.showAlert('Error!','Estamos presentando errores con los servicios. Intente más tarde.');
+          this.showAlert(
+            "Error!",
+            "Estamos presentando errores con los servicios. Intente más tarde."
+          );
           break;
         }
         default: {
-          this.showAlert('Error!',e.error.msg);
+          this.showAlert("Error!", e.error.msg);
           break;
         }
       }
-    }else{
-      if(e.message){
-        this.showAlert('Error!',e.error.msg);
-      }else{
-        this.showAlert('Error!',e);
+    } else {
+      if (e.message) {
+        this.showAlert("Error!", e.error.msg);
+      } else {
+        this.showAlert("Error!", e);
       }
     }
   }
-  private handleErr(e){
+  private handleErr(e) {
     console.log(e);
-    if(e.status){
-      switch(e.status){
-        case 0:{
-          this.showAlert('Error!','Usted está trabajando sin internet. Revise su conexión.');
+    if (e.status) {
+      switch (e.status) {
+        case 0: {
+          this.showAlert(
+            "Error!",
+            "Usted está trabajando sin internet. Revise su conexión."
+          );
           break;
         }
-        case 401:{
-          this.showAlert('Error!','Acceso no autorizado.');
+        case 401: {
+          this.showAlert("Error!", "Acceso no autorizado.");
           break;
         }
-        case 404:{
-          this.showAlert('Error!','Recurso no encontrado. Contacte al administrador.');
+        case 404: {
+          this.showAlert(
+            "Error!",
+            "Recurso no encontrado. Contacte al administrador."
+          );
           break;
         }
         case 500: {
-          this.showAlert('Error!','Estamos presentando errores con los servicios. Intente más tarde.');
+          this.showAlert(
+            "Error!",
+            "Estamos presentando errores con los servicios. Intente más tarde."
+          );
           break;
         }
         default: {
-          this.showAlert('Error!',e.error.message);
+          this.showAlert("Error!", e.error.message);
           break;
         }
       }
-    }else{
-      if(e.message){
-        this.showAlert('Error!',e.error.msg);
-      }else{
-        this.showAlert('Error!',e);
+    } else {
+      if (e.message) {
+        this.showAlert("Error!", e.error.msg);
+      } else {
+        this.showAlert("Error!", e);
       }
     }
   }
